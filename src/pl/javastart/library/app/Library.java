e7f9140b34727e9ec92feabc015f15ea66e9b11d
AnnaDK <annadktester@gmail.com>
2023-03-21 15:16:09 +0100

    Structure changed, if instruction added at Book class

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,6 +1,9 @@
-class Library {
+package pl.javastart.library.app;
+import pl.javastart.library.model.Book;
+
+public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("Pan Samochodzik i Templariusze ", "Zbigniew Nienacki",      1966,      223, "Oficyna Wydawnicza WARMIA", "9788385875031");
         Book book2 = new Book("Podróż za jeden uśmiech", "Adam Bahdaj", 1973, 214, "Wydawnictwo Literatura",
@@ -8,7 +11,7 @@ class Library {
         Book book3 = new Book("Wilki", "Adam Wajrak",
                 2015, 272, "Agora", "9788326823053");
 
-        System.out.print(appName);
+        System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
         book1.printInfo();
         book2.printInfo();
